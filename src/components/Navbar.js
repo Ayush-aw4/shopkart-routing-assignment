d224a0ad1d6dac466c776a3a9b0815e0ed3716f1
varnikag24 <143873214+varnikag24@users.noreply.github.com>
2026-04-04 12:52:08 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,14 @@
 import React from "react";
 
-// TODO: Convert to Link or NavLink
-
 function Navbar() {
   return (
-    <nav style={{ padding: "10px", background: "#eee" }}>
-      <span style={{ marginRight: "15px" }}>Home</span>
-      <span style={{ marginRight: "15px" }}>About</span>
-      <span>Contact</span>
+    <nav>
+      {/* TODO: Replace <a> with Link */}
+      
+      <a href="/">Home</a>
+      <a href="/about">About</a>
+      <a href="/contact">Contact</a>
+      
     </nav>
   );
 }
